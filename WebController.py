bc0594d3c04a45643ee5704916b046d669ff9a29
ForestHouse <ForestHouse2316@users.noreply.github.com>
2021-06-17 12:36:08 +0900

    Initial inner commit

--- WebController.py
+++ WebController.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import urllib
 import os
 import time
 import threading
+
+# Declare element extractor
+soup = bs('html', 'html.parser')
+
+
 
 
 # Init browser
@@ -10,11 +17,19 @@ driver.maximize_window()
 
 while True:
     command = input()
-    if command = 'exit':
+    if command == 'exit':
         break
-    elif command = '':
-        print(" page source . . .")
+    elif command == '':
+        print("Getting page source . . .")
+        source = driver.page_source
         print("Analyzing page source . . .")
+        img_elements = soup.find_all("img") # image elements list
+        print(img_elements)
+        img_src = []
+        for element in img_elements:
+            img_src.append(element.attrs['src'])
+        print("Downloading image resources . . .")
+        urllib.urlretrieve(img_src, img_src)
 
 
 
